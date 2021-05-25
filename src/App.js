70fba19ac4cc589cfdae608d7b812806c01019c1
erdldncr <erdldncr@gmail.com>
2021-05-25 11:40:24 +0100

    project completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import ImageCard from "./components/ImageCard";
+import ImageSearch from "./components/ImageSearch";
 
 
 function App() {
@@ -6,47 +8,36 @@ function App() {
   const[isLoading,setLoading]=useState(true)
   const[term,setTerm]=useState('')
   useEffect(()=>{
+    setLoading(true)
     fetch(`https://pixabay.com/api/?key=21568865-bcfa69eefbf26879c6939fcbf&q=${term}&image_type=photo&pretty=true`)
     .then(res=>res.json())
     .then(data=>{
-      console.log(data)
+      setImages(data.hits)
+      setLoading(false)
     })
     .catch(err=>console.log(err))
-  },[])
+ 
+  },[term])
+ const handleSubmit=(text)=>{
+   
+   setTerm(text)
+ }
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-    <img src="https://source.unsplash.com/random" alt="" className="w-full"/>
-    <div className="px-6 py-4">
-      <div className="font-bold text-purple-500 text xl mb-2">
-        Photo by Jogn Erdal
-      </div>
-      <ul>
-        <li>
-          <strong>Views:</strong>
-          4000
-        </li>
-        <li>
-          <strong>Downloads:</strong>
-          300
-        </li>
-        <li>
-          <strong>Likes:</strong>
-          400
-        </li>
-      </ul>
+  
+    <div className="container mx-auto">
+      <ImageSearch handleSubmit={handleSubmit} />
+      {!isLoading&&images.length===0&&  <h1 className="text-5xl text-center mx-auto mt-32">No images found</h1>  }
+     {isLoading? 
+      <h1 className="text-6xl text-center mx-auto mt-32">Loading.....</h1>
+     :  <div className="grid grid-cols-3 gap-4">
+          {
+            images.map(image=>(
+              <ImageCard  key={image.id} {...image} />
+            ))
+          }
+      </div>}
     </div>
-    <div className="px-6 py-4">
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-        #tag1
-      </span>
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-        #tag1
-      </span>
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-        #tag1
-      </span>
-    </div>
-    </div>
+     
   );
 }
 
